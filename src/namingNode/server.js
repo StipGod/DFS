8e32532e8adfa6b8ea5afe53790a6764f305cdf5
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-10-01 10:21:06 -0500

    setup server

--- src/namingNode/server.js
+++ src/namingNode/server.js
@@ -2,27 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const findController = require('./controllers/findController');
+const resourceController = require('./controllers/resourceController');
 
 function serve(port, host) {
   const app = express();
   app.use(bodyParser.json());
 
   app.get('/find', findController.find);
-
-  app.post('/resource', (req, res) => {
-    // send the ip of the data node that will save the file. only send back the ip of the leader of that file. that leader will know here to save the replica
-    const data = req.body;
-    res.send(``);
-  });
-
-  app.put('/resource', (req, res) => {
-    // ither send the ip of the leader node that has that file or respond with a file not found
-    // if the file is found send the ip of the leader node that has that file
-    // that leader will know here the replica is saved
-    const id = req.params.id;
-    const data = req.body;
-    res.send(``);
-  });
+  app.post('/resource',resourceController.createResource);
+  app.put('/resource',resourceController.updateResource);
 
   app.listen(port, () => {
     console.log(`Naming Node on http://${host}:${port}`);

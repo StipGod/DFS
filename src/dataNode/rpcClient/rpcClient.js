b1748802863dbe942ddd2ef010cd3fea565acd6e
juan-c-s <juancamilo.salazar2002@gmail.com>
2023-10-01 23:03:21 -0500

    DataNodeToDataNodeConfig And Buffer Transfering

--- src/dataNode/rpcClient/rpcClient.js
+++ src/dataNode/rpcClient/rpcClient.js
@@ -3,6 +3,9 @@ const grpc =require( '@grpc/grpc-js');
 const path = require('path');
 const dotenv = require('dotenv');
 
+const fs = require('fs');
+
+
 dotenv.config({ path: path.join(__dirname, '../../../.env') });
 
 const fileManager = require('../../grpcConfig/fileManager'); 
@@ -10,8 +13,8 @@ const fileManager = require('../../grpcConfig/fileManager');
 
 const {NAMING_NODE_HOST, RPC_PORT} = process.env
 
+const client = new fileManager(`${NAMING_NODE_HOST}:${RPC_PORT}`,grpc.credentials.createInsecure());
  const createFile = (storageIps)=>{
-  const client = new fileManager(`${NAMING_NODE_HOST}:${RPC_PORT}`,grpc.credentials.createInsecure());
   console.log(storageIps)
   client.createFile({storageIps}, (err, data) => {
     if(err){
@@ -21,8 +24,28 @@ const {NAMING_NODE_HOST, RPC_PORT} = process.env
     }
   }); 
 }
+const uploadFile = (fileName)=>{
 
-createFile(["0.0.1.1","0.0.2.1"]);
+  const fileStream = fs.createReadStream(`../filesStorage/${fileName}`);
+  const call = client.UploadFile((error, response) => {
+    if (response.success) {
+        console.log('File uploaded successfully.');
+        //LLAMAR AL NAMING NODE PARA ACTUALIZAR HASHMAP
+    } else {
+        console.error(`Error: ${response.message}`);
+    }
+  });
+  fileStream.on('data', (chunk) => {
+    call.write({fileName, chunk });
+  });
+
+  fileStream.on('end', () => {
+    call.end();
+  });
+}
+
+// uploadFile("fall.jpg");
+// createFile(["0.0.1.1","0.0.2.1"]);
 module.exports ={
     createFile
 }
